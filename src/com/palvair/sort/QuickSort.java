cbfaa530046382f0480b34a59b13299145865310
rpalvair <r.palvair@gmail.com>
2016-07-27 16:52:28 +0200

    Added comments + extract method permut

---
+++ src/com/palvair/sort/QuickSort.java
@@ -0,0 +1,54 @@
+package com.palvair.sort;
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+	public static int[] tab = {101,115,30,63,47};
+	
+	public static void main(String[] args) {
+		quicksort(tab, 0,4);
+		System.out.println("tab = "+Arrays.toString(tab));
+	}
+	
+
+	
+	public static void quicksort(int [] tableau, int début, int fin) {
+	    if (début < fin) {
+	        int indicePivot = partition(tableau, début, fin);
+	        quicksort(tableau, début, indicePivot-1);
+	        quicksort(tableau, indicePivot+1, fin);
+	    }
+	}
+
+	public static int partition (int [] t, int début, int fin) {
+	    //choix pivot au début
+		int valeurPivot = t[début];
+	    int gauche = début+1;
+	    int droite = fin;
+	    //pas de chevauchement
+	    while (gauche < droite) {
+	    	//positionne les éléments inférieurs au pivot à gauche et les éléments supérieurs au pivot à droite
+	        while(gauche < droite && t[droite] >= valeurPivot) droite--;
+	        while(gauche < droite && t[gauche] <= valeurPivot) gauche++;
+
+	        //permutation
+	        if(gauche != droite) {
+	        	t = permut(t, gauche, droite);
+	        }
+	        
+	    }
+	    //je teste l'élément courant de la partie gauche donc juste après le pivot si je n'ai pas avancé
+	    if (t[gauche] > valeurPivot) gauche--; //ici je trouve un élément inférieur au pivot ou le pivot lui-même
+	    t[début] = t[gauche]; //positionne l'élément courant(le plus petit élément) au début du tableau
+	    t[gauche] = valeurPivot; //déplace le pivot à la place de l'élément courant
+	    return gauche;
+	}
+	
+	public static int[] permut(int[] tab,int gauche, int droite) {
+		int temp = tab[gauche];
+		tab[gauche] = tab[droite];
+		tab[droite] = temp;
+		return tab;
+	}
+}
