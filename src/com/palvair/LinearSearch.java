40d34370688746af75eb8c0b921c2339affe5445
rpalvair <r.palvair@gmail.com>
2016-07-30 14:46:45 +0200

    Added recursive function

--- src/com/palvair/LinearSearch.java
+++ src/com/palvair/LinearSearch.java
@@ -4,12 +4,17 @@ public class LinearSearch {
 
 	public static void main(String[] args) {
 		int[] tab = { 101, 115, 30, 63, 47, 20 };
-		boolean isPresent = isPresent(tab, 47);
+		int el = 20;
+		boolean isPresent = isPresent(tab, el);
 		System.out.println("isPresent = " + isPresent);
+		
+		boolean isPresentRecursive = isPresentRecursive(tab, el);
+		System.out.println("isPresentRecursive = " + isPresentRecursive);
 	}
 
 	public static int getIndice(int[] tab, int el) {
 		for (int i = 0; i < tab.length; i++) {
+			//invariant de boucle : si el est présent dans tab, il est forcément présent dans tab[i+1]
 			if (tab[i] == el) {
 				return i;
 			}
@@ -23,4 +28,27 @@ public class LinearSearch {
 		}
 		return false;
 	}
+	
+	public static int getIndiceRecursive(int[] tab , int i, int el) {
+		//cas de base
+		if(i >= tab.length) {
+			return -1;
+		} else {
+			if(tab[i] == el) {
+				return i;
+			} else {
+				return getIndiceRecursive(tab, i+1, el);
+			}
+		}
+	}
+	
+	public static boolean isPresentRecursive(int[] tab, int el) {
+		int start = 0;
+		if(getIndiceRecursive(tab, start, el) != -1) {
+			return true;
+		}
+		return false;
+	}
+	
+
 }
